b3287d9b86fcb18eba02754772eb61b62d3ba1bf
elecdeer <37701077+elecdeer@users.noreply.github.com>
2026-03-23 13:38:31 +0900

    perf: speed up ghwt pr picker

--- hanger/plugins/ghwt/ghwt.plugin.zsh
+++ hanger/plugins/ghwt/ghwt.plugin.zsh
@@ -25,55 +25,68 @@ function ghwt() {
     return
   fi
 
-  local worktrees=$(git worktree list --porcelain | grep -E '^branch' | sed 's|branch refs/heads/||')
+  local worktrees
+  worktrees=$(git worktree list --porcelain | awk '/^branch refs\/heads\// { sub(/^branch refs\/heads\//, ""); print }')
   
   # セッションキャッシュを使用（まだ取得されていない場合はその場で取得）
   local current_user="${_GHWT_CURRENT_USER:-$(gh api user --jq '.login')}"
-  
-  local selected_pr=$(gh pr list --json number,title,headRefName,statusCheckRollup,createdAt,author,reviewRequests \
-    | jq -jr --arg worktrees "$worktrees" --arg current_user "$current_user" '
-      .[] | 
-      .headRefName as $branch | 
-      (now - (.createdAt | fromdateiso8601)) as $diff | 
-      (if $diff < 3600 then 
-        ($diff / 60 | floor | tostring) + "m ago"
-       elif $diff < 86400 then 
-        ($diff / 3600 | floor | tostring) + "h ago"
-       elif $diff < 2592000 then 
-        ($diff / 86400 | floor | tostring) + "d ago"
-       elif $diff < 31536000 then 
-        ($diff / 2592000 | floor | tostring) + "mo ago"
-       else 
-        ($diff / 31536000 | floor | tostring) + "y ago"
-       end) as $relative | 
-      (.statusCheckRollup // [] | 
-        if length == 0 then " " 
+  local pr_cache_file
+  pr_cache_file=$(mktemp -t ghwt-prs.XXXXXX) || return 1
+
+  if ! gh pr list --json number,title,headRefName,statusCheckRollup,updatedAt,author,reviewRequests,additions,deletions,changedFiles,body > "$pr_cache_file"; then
+    rm -f "$pr_cache_file"
+    return 1
+  fi
+
+  export GHWT_PR_CACHE="$pr_cache_file"
+
+  local selected_branch
+  selected_branch=$(jq -jr --arg worktrees "$worktrees" --arg current_user "$current_user" '
+      sort_by(.updatedAt) | reverse | .[] |
+      .headRefName as $branch |
+      (now - (.updatedAt | fromdateiso8601)) as $diff |
+      (if $diff < 3600 then
+        "about " + (($diff / 60 | floor | if . < 1 then 1 else . end) | tostring) + " minutes ago"
+       elif $diff < 86400 then
+        "about " + (($diff / 3600 | floor) | tostring) + " hours ago"
+       elif $diff < 2592000 then
+        "about " + (($diff / 86400 | floor) | tostring) + " days ago"
+       elif $diff < 31536000 then
+        "about " + (($diff / 2592000 | floor) | tostring) + " months ago"
+       else
+        "about " + (($diff / 31536000 | floor) | tostring) + " years ago"
+       end) as $relative |
+      (.statusCheckRollup // [] |
+        if length == 0 then " "
         elif all(.[]; .conclusion == "SUCCESS") then "\u001b[32m✓\u001b[0m"
         elif any(.[]; .conclusion == "FAILURE") then "\u001b[31m✗\u001b[0m"
-        else "\u001b[33m○\u001b[0m" 
-        end) as $ci | 
+        else "\u001b[33m○\u001b[0m"
+        end) as $ci |
       ((.reviewRequests // [] | map(.login) | any(. == $current_user)) as $is_reviewer |
-        if $is_reviewer then " 󰭑" else "  " end) as $review_icon | 
-      (if ($worktrees | split("\n") | any(. == $branch)) then "\u001b[32m\u001b[0m " 
-       else "" 
-       end) as $wt_icon | 
-      "\u001b[36m#" + (.number | tostring) + "\u001b[0m " + $ci + $review_icon + " " + 
-      "\u001b[34m" + $branch + "\u001b[0m " + $wt_icon + 
-      "\u001b[90m" + $relative + "\u001b[0m " + 
-      "\u001b[35m" + .author.login + "\u001b[0m" + 
-      "\n  " + .title + "\u0000"' \
-    | fzf --read0 --gap \
+        if $is_reviewer then " 󰭑" else "  " end) as $review_icon |
+      (if ($worktrees | split("\n") | any(. == $branch)) then "\u001b[32m\u001b[0m "
+       else ""
+       end) as $wt_icon |
+      (.number | tostring) + "\t" + $branch + "\t" +
+      "\u001b[36m#" + (.number | tostring) + "\u001b[0m " + $ci + $review_icon + " " +
+      "\u001b[34m" + $branch + "\u001b[0m " + $wt_icon +
+      "\u001b[90m" + $relative + "\u001b[0m " +
+      "\u001b[35m" + .author.login + "\u001b[0m" +
+      "\n  " + .title + "\u0000"' "$pr_cache_file" |
+    fzf --read0 --gap \
+      --delimiter $'\t' \
+      --with-nth 3.. \
       --header "Select PR" \
-      --preview 'pr_num=$(echo {1} | sed "s/#//"); gh pr view $pr_num --json additions,deletions,changedFiles | jq -r "\"\\u001b[33m+\" + (.additions | tostring) + \"\\u001b[0m \\u001b[31m-\" + (.deletions | tostring) + \"\\u001b[0m \\u001b[90m(\" + (.changedFiles | tostring) + \" files)\\u001b[0m\n\""; gh pr view $pr_num --json body --jq ".body" | bat --paging=never -l markdown --color=always --style=plain' \
-      --preview-window 'right:30%:wrap' \
-    | head -1 \
-    | sed 's/\x1b\[[0-9;]*m//g' \
-    | awk '{print $1}' \
-    | sed 's/#//')
-  
-  if [[ -n "$selected_pr" ]]; then
-    local branch_name=$(gh pr view "$selected_pr" --json headRefName --jq '.headRefName')
-    print -s "git wt \"$branch_name\""
-    git wt "$branch_name"
+      --preview 'pr_num={1}; jq -r --argjson pr "$pr_num" '\''first(.[] | select(.number == $pr) | "\u001b[33m+" + (.additions | tostring) + "\u001b[0m \u001b[31m-" + (.deletions | tostring) + "\u001b[0m \u001b[90m(" + (.changedFiles | tostring) + " files)\u001b[0m")'\'' "$GHWT_PR_CACHE"; jq -r --argjson pr "$pr_num" '\''first(.[] | select(.number == $pr) | (.body // ""))'\'' "$GHWT_PR_CACHE" | bat --paging=never -l markdown --color=always --style=plain' \
+      --preview-window 'right:30%:wrap' |
+    head -1 |
+    cut -f2)
+
+  rm -f "$pr_cache_file"
+  unset GHWT_PR_CACHE
+
+  if [[ -n "$selected_branch" ]]; then
+    print -s "git wt \"$selected_branch\""
+    git wt "$selected_branch"
   fi
 }
