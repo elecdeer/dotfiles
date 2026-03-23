b6d6146ff3b134a5082e33a555aea0a2a1fe7d7f
elecdeer <37701077+elecdeer@users.noreply.github.com>
2026-03-23 13:47:56 +0900

    feat: add fzf ghq search plugin

---
+++ hanger/plugins/fzf-ghq-search/fzf-ghq-search.plugin.zsh
@@ -0,0 +1,33 @@
+#!/usr/bin/env zsh
+
+_fzf_ghq_search_plugin_dir="${0:A:h}"
+
+function fzf-ghq-search() {
+  setopt local_options
+  unsetopt xtrace
+
+  local ghq_root repo
+  ghq_root=$(ghq root) || return 1
+
+  repo=$(
+    GHQ_ROOT="$ghq_root" "$_fzf_ghq_search_plugin_dir/executable_fzf-ghq-list" "$ghq_root" \
+      | fzf --prompt="repository > " --ansi \
+          --delimiter $'\t' \
+          --with-nth 2,3 \
+          --nth 1 \
+          --preview 'if [[ -f "$GHQ_ROOT"/{2}/README.md ]]; then bat --color=always --style=numbers "$GHQ_ROOT"/{2}/README.md; else lsd -1 --icon=always --color=always "$GHQ_ROOT"/{2}; fi' \
+          --preview-window=right:50% \
+          | cut -f2
+  )
+
+  if [[ -n "$repo" ]]; then
+    BUFFER+="cd $ghq_root/$repo"
+    zle accept-line
+  else
+    zle redisplay
+    return 1
+  fi
+}
+
+zle -N fzf-ghq-search
+bindkey $'\e[113;6u' fzf-ghq-search
